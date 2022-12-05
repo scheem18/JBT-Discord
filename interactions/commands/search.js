b0577bbf373f006d2f329acc8a0d47faa0b00050
scheem18 <kome1208@outlook.jp>
2022-12-05 14:27:18 +0900

    canister v1

--- interactions/commands/search.js
+++ interactions/commands/search.js
@@ -41,21 +41,19 @@ module.exports = {
         if (interaction.options.getSubcommand() === 'tweaks') {
             await interaction.deferReply();
             try {
-                const pkg = (await (axios.get(`https://api.canister.me/v2/jailbreak/package/search?q=${query}`))).data.data[0];
+                const pkg = (await (axios.get(`https://api.canister.me/v1/community/packages/search?query=${encodeURIComponent(query)}&limit=1&responseFields=name,description,author,price,packageIcon,depiction,repository.name,repository.uri,latestVersion,identifier`))).data.data[0];
                 if (!pkg) return await interaction.editReply({content:'パッケージが見つかりませんでした。'});
-                const repo = (await (axios.get(`${pkg.refs.repo}`))).data.data;
                 const embed = new EmbedBuilder()
-                .setAuthor({name:`${repo.name}`,iconURL:`${repo.uri}/CydiaIcon.png`,url:`${repo.uri}`})
-                .setTitle(pkg.name ?? pkg.package)
+                .setAuthor({name:`${pkg.repository.name}`,iconURL:`${pkg.repository.uri}/CydiaIcon.png`,url:`${pkg.repository.uri}`})
+                .setTitle(pkg.name ?? pkg.identifier)
                 .setDescription(`${pkg.description.length > 4000 ? pkg.description.slice(0,4000) : pkg.description}` ?? null)
-                .setThumbnail(pkg.icon?.startsWith('file://') ? null : pkg.icon ?? null)
+                .setThumbnail(pkg.packageIcon?.startsWith('file://') ? null : pkg.packageIcon)
                 .addFields(
-                    {name:'パッケージID',value:pkg.package},
-                    {name:`作成者`,value:`${pkg.author}`,inline:true},
-                    {name:'バージョン',value:`${pkg.version}`,inline:true},
-                    {name:'リポジトリ',value:`[${repo.name}](${repo.uri})`,inline:true},
+                    {name:'パッケージID',value:pkg.identifier},
+                    {name:'作成者',value:`${pkg.author}`,inline:true},
+                    {name:'バージョン',value:`${pkg.latestVersion}`,inline:true},
+                    {name:'リポジトリ',value:`[${pkg.repository.name}](${pkg.repository.uri})`,inline:true},
                     {name:'価格',value:pkg.price,inline:true},
-                    {name:'カテゴリー',value:pkg.section,inline:true},
                 )
                 .setFooter({text:'Powered by canister.me'})
                 await interaction.editReply({embeds:[embed]});
