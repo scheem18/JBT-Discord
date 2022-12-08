b87075c69fc84e2a9ae0449ac493e79cc6af0783
scheem18 <kome1208@outlook.jp>
2022-12-08 23:34:23 +0900

    remove can i jb

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -21,36 +21,6 @@ module.exports = {
         });
       }
       await context.run({ client, interaction });
-    } else if (interaction.isAutocomplete()) {
-      if (interaction.commandName === 'canijailbreak') {
-        try {
-          const focused = interaction.options.getFocused(true);
-          const data = JSON.parse(fs.readFileSync('main.json'));
-          let filtered;
-          if (focused.name === 'device') {
-            const devices = data.group.filter(device => device.type === 'iPhone');
-            const list = devices.map(device => ({name:device.name, value:device.name}));
-            filtered = list.filter(x => (x.name.startsWith(focused.value)));
-            while (filtered.length > 25) {
-                filtered.pop()
-            }
-          }
-          if (focused.name === 'version') {
-            const input_device = interaction.options.getString('device');
-            const device = data.group.filter(group => (group.type === 'iPhone' && group.key.toLowerCase() === input_device.toLowerCase()));
-            const versions = data.ios.filter(x => (x.osStr === 'iOS' && x.deviceMap.includes(device[0].devices[0]))).map(x => {
-              return {name:x.version, value:x.version}
-            })
-            filtered = versions.filter(x => (x.name.startsWith(focused.value))) 
-            while (filtered.length > 25) {
-              filtered.pop();
-            }
-          }
-          await interaction.respond(filtered);
-        } catch (err) {
-          console.error(err);
-        }
-      }
     }
 	}
 }
