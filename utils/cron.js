263ccdf94714c3a6dd49bd6f26ddbeb21aad13d5
scheem18 <kome1208@outlook.jp>
2023-01-07 16:09:43 +0900

    fix

--- utils/cron.js
+++ utils/cron.js
@@ -5,8 +5,8 @@ function cron (client) {
     axios.get('https://dhinakg.github.io/check-pallas/minified-v3.json').then(({data}) => {
         fs.writeFileSync('./json/delay.json', JSON.stringify(data), 'utf-8');
     }).catch(console.error);
-    axios.get('https://dhinakg.github.io/check-pallas/minified-v3.json').then(({data}) => {
-        fs.writeFileSync('./json/delay.json', JSON.stringify(data), 'utf-8');
+    axios.get('https://api.appledb.dev/main.json').then(({data}) => {
+        fs.writeFileSync('./json/main.json', JSON.stringify(data), 'utf-8');
     }).catch(console.error);
     require('./sigscheck').sigscheck(client);
     setInterval(() => {
