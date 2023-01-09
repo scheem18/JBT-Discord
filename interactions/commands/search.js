7a64e493c860a42f6c62d03cbe2ab8184dab87ee
scheem18 <kome1208@outlook.jp>
2023-01-09 17:45:52 +0900

    canister v2に変更

--- interactions/commands/search.js
+++ interactions/commands/search.js
@@ -43,26 +43,33 @@ module.exports = {
             await interaction.deferReply();
             const controller = new InteractionEmbedControl(interaction);
             try {
-                const pkgs = (await (axios.get(`https://api.canister.me/v1/community/packages/search?query=${encodeURIComponent(query)}&searchFields=name,identifier,description&limit=5&responseFields=*,repository.name,repository.uri`))).data.data;
-                if (!pkgs[0]) return await message.reply({content:`${query}に一致するパッケージが見つかりませんでした。`});
-                const embeds = pkgs.map(pkg => {
+                const results = []
+                const { data } = await axios.get(`https://api.canister.me/v2/jailbreak/package/search?q=${query}&limit=250`);
+                if (data.count <= 0) return await message.reply({content:`${query}に一致するパッケージが見つかりませんでした。`});
+                data.data.forEach((pkg) => {
+                    if (results.findIndex(result => (result.package === pkg.package)) === -1) {
+                        results.push(pkg);
+                    }
+                });
+                const embeds = results.map(pkg => {
                     let color = parseInt(pkg.tintColor?.replace('#',""),16);
                     if (isNaN(color)) {
                         color = null;
                     }
-                    return (new EmbedBuilder()
+                    return new EmbedBuilder()
                     .setAuthor({name:`${pkg.repository.name} | Powered by Canister`,iconURL:`${pkg.repository.uri}/CydiaIcon.png`,url:`${pkg.repository.uri}`})
-                    .setTitle(pkg.name ?? pkg.identifier)
-                    .setDescription(`${pkg.description.length > 4000 ? pkg.description.slice(0,4000) : pkg.description}` ?? null)
-                    .setThumbnail(pkg.packageIcon?.startsWith('file://') ? null : pkg.packageIcon)
+                    .setTitle(pkg.name ?? pkg.package)
+                    .setDescription(pkg.description?.length > 4096 ? pkg.description.slice(0,4096) : pkg.description)
+                    .setThumbnail(pkg.icon?.startsWith('file://') ? null : pkg.icon)
                     .setColor(color)
                     .addFields(
-                        {name:'パッケージID',value:pkg.identifier},
+                        {name:'パッケージID',value:pkg.package},
                         {name:'作成者',value:`${pkg.author}`,inline:true},
-                        {name:'バージョン',value:`${pkg.latestVersion}`,inline:true},
+                        {name:'バージョン',value:`${pkg.version}`,inline:true},
+                        {name:'セクション',value:pkg.section,inline:true},
                         {name:'リポジトリ',value:`[${pkg.repository.name}](${pkg.repository.uri})`,inline:true},
                         {name:'価格',value:pkg.price,inline:true}
-                    ))
+                    );
                 });
                 await controller.reply(embeds, {deferred:true});
             } catch (err) {
